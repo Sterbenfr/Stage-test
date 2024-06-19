1cc3ad8074aa2c1f20f6d13233fcb41784c61aa0
Aurelien <schmieder.aurelien@gmail.com>
2024-06-19 11:10:26 +0200

    feat(interactionID):Adding interaction ID

--- migration/Table-Interractions.sql
+++ migration/Table-Interractions.sql
@@ -30,3 +30,29 @@ INSERT INTO Interactions (
 (2, 2, '2023-06-02', 'PRE', 'TEL', 2, "Deuxième interaction avec l'entité 102", NULL, '2023-06-16'),
 (3, 3, '2023-06-03', 'REL', 'SMS', 3, "Troisième interaction avec l'entité 103", NULL, '2023-06-17'),
 (4, 4, '2023-06-04', 'PRE', 'MAI', 4, "Quatrième interaction avec l'entité 104", NULL, '2023-06-18');
+
+SELECT 
+    i.code_Utilisateur_Prospecteur, 
+    i.code_Entite_Prospectee, 
+    i.date_interaction, 
+    i.code_type_interactions, 
+    i.code_modalite_interaction, 
+    i.code_contact_entite, 
+    i.commentaires, 
+    i.pieces_associees, 
+    i.date_relance
+FROM 
+    Interactions i
+LEFT JOIN 
+    Utilisateurs u ON i.code_Utilisateur_Prospecteur = u.code_utilisateur
+LEFT JOIN 
+    Entite e ON i.code_Entite_Prospectee = e.code_Entite
+LEFT JOIN 
+    TypeInteractions t ON i.code_type_interactions = t.code_type_interaction
+LEFT JOIN 
+    ModaliteInteractions m ON i.code_modalite_interaction = m.code_modalite_interaction
+LEFT JOIN 
+    ContactEntite c ON i.code_contact_entite = c.code_utilisateur_suivant
+WHERE 
+    i.code_Utilisateur_Prospecteur = 1;
+
