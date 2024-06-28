6ad73d8aaf0e054524ff956ff8100119aa271513
Alexis aka Yazm <alexishazebrouck503@gmail.com>
2024-06-28 15:57:50 +0200

    css for recep and ML

--- app/dons/[donsID]/reception/[receptionID]/page.tsx
+++ app/dons/[donsID]/reception/[receptionID]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
+import style from '../../../../../styles/components.module.css'
 
 interface ReceptionID {
     numero_reception: number
@@ -47,24 +48,135 @@ export default function ReceptionPage({
     if (!reception || reception.length === 0) return <div>Loading...</div>
 
     return (
-        <div>
-            <h1>reception</h1>
-            <p>{reception[0].numero_reception}</p>
-            <p>{reception[0].code_Don}</p>
-            <p>{reception[0].numero_BL}</p>
-            <p>
-                {reception[0].date_reception == null
-                    ? ''
-                    : reception[0].date_reception.toString().split('T')[0]}
-            </p>
-            <p>{reception[0].heure_reception}</p>
-            <p>{reception[0].nombre_palettes_recues}</p>
-            <p>{reception[0].nombre_palettes_consignees_recues}</p>
-            <p>{reception[0].nombre_palettes_consignees_rendues}</p>
-            <p>{reception[0].nombre_cartons_recus}</p>
-            <p>{reception[0].poids_recu_kg}</p>
-            <p>{reception[0].produits_sur_palettes}</p>
-            <p>{reception[0].commentaires}</p>
+        <div className={style.idPage}>
+            <div>
+                <h1>Récéption</h1>
+            </div>
+            <div className={style.info_id}>
+                <div className={style.col_1}>
+                    <div className={style.info}>
+                        <p className={style.titre}>Numero de reception :</p>
+                        <p>
+                            {reception[0].numero_reception == null
+                                ? '/'
+                                : reception[0].numero_reception}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Code de don :</p>
+                        <p>
+                            {reception[0].code_Don == null
+                                ? '/'
+                                : reception[0].code_Don}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Numero BL :</p>
+                        <p>
+                            {reception[0].numero_BL == null
+                                ? '/'
+                                : reception[0].numero_BL}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Date de reception :</p>
+                        <p>
+                            {reception[0].date_reception == null
+                                ? '/'
+                                : reception[0].date_reception
+                                      .toString()
+                                      .split('T')[0]}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Heure de reception :</p>
+                        <p>
+                            {reception[0].heure_reception == null
+                                ? '/'
+                                : reception[0].heure_reception}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>
+                            Nombre de palettes recues :
+                        </p>
+                        <p>
+                            {reception[0].nombre_palettes_recues == null
+                                ? '/'
+                                : reception[0].nombre_palettes_recues}
+                        </p>
+                    </div>
+                </div>
+                <div className={style.col_2}>
+                    <div className={style.info}>
+                        <p className={style.titre}>
+                            Nombre palettes consignées recues :
+                        </p>
+                        <p>
+                            {reception[0].nombre_palettes_consignees_recues ==
+                            null
+                                ? '/'
+                                : reception[0]
+                                      .nombre_palettes_consignees_recues}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>
+                            Nombre de palettes consignées rendues :
+                        </p>
+                        <p>
+                            {reception[0].nombre_palettes_consignees_rendues ==
+                            null
+                                ? '/'
+                                : reception[0]
+                                      .nombre_palettes_consignees_rendues}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Nombre de cartons recus :</p>
+                        <p>
+                            {reception[0].nombre_cartons_recus == null
+                                ? '/'
+                                : reception[0].nombre_cartons_recus}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>Poids recu en kg :</p>
+                        <p>
+                            {reception[0].poids_recu_kg == null
+                                ? '/'
+                                : reception[0].poids_recu_kg}
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.titre}>
+                            Produits sur les palettes :
+                        </p>
+                        <p>
+                            {reception[0].produits_sur_palettes == null
+                                ? '/'
+                                : reception[0].produits_sur_palettes}
+                        </p>
+                    </div>
+                    <div className={style.info}>
+                        <p className={style.titre}>commentaires :</p>
+                        <p>
+                            {reception[0].commentaires == null
+                                ? '/'
+                                : reception[0].commentaires}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
